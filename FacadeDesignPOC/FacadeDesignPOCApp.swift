fb84abcd411bd47fba960956269ca9be99ccdeb6
Jonathon Lannon <jonathonlannon@icloud.com>
2022-10-26 13:32:04 -0700

    Initial Commit

---
+++ FacadeDesignPOC/FacadeDesignPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  FacadeDesignPOCApp.swift
+//  FacadeDesignPOC
+//
+//  Created by Jonathon Lannon on 10/26/22.
+//
+
+import SwiftUI
+
+@main
+struct FacadeDesignPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
